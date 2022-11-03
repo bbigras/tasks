510d0e4a9fbcc68f01e3362eb821d5e11886812e
Bruno Bigras <bigras.bruno@gmail.com>
2022-11-03 11:38:49 -0400

    wearos hello world

---
+++ myapplication/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+repositories {
+    mavenCentral()
+    google()
+    maven {
+        url = uri("https://jitpack.io")
+        content {
+            includeGroup("com.github.tasks")
+            includeModule("com.github.bitfireAT", "cert4android")
+            includeModule("com.github.tasks.opentasks", "opentasks-provider")
+            includeModule("com.github.QuadFlask", "colorpicker")
+            includeModule("com.github.twofortyfouram", "android-plugin-api-for-locale")
+            includeModule("com.github.franmontiel", "PersistentCookieJar")
+        }
+    }
+}
+
+android {
+    namespace = "com.example.myapplication"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdk = 30
+        targetSdk = 30
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("com.google.android.gms:play-services-wearable:17.1.0")
+    implementation("androidx.percentlayout:percentlayout:1.0.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.wear:wear:1.2.0")
+}
